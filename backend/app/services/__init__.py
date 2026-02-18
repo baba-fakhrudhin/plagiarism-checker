28583ef27854de731add54b7d88dbbf2bc8800a7
Roopanagudi Baba Fakhrudhin <ibabaprakurddin@gmail.com>
2026-02-18 08:51:50 +0530

    Initial Commit for constructor

--- backend/app/services/__init__.py
+++ backend/app/services/__init__.py
@@ -1,6 +1,1 @@
-from .file_processor import FileProcessor
-from .plagiarism_detector import PlagiarismDetector
-from .web_scraper import WebScraper
-from .ai_detector import AIDetector
-
-__all__ = ['FileProcessor', 'PlagiarismDetector', 'WebScraper', 'AIDetector']
+from .detection_engine import DetectionEngine
